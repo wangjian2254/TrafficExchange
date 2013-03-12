818d7db2e8720b7523ed7d17f2646e0a1da4ab44
王健 <wangjian2254@gmail.com>
2013-03-13 00:06:48 +0800

    新的设计

--- traffic/views.py
+++ traffic/views.py
@@ -1,7 +1,26 @@
 #coding=utf-8
 # Create your views here.
+from TrafficExchange.traffic.tools import custom_render_to_response
 
-from django.shortcuts import render_to_response
 
 def index(request):
-    return render_to_response('index.html',{})
+    return custom_render_to_response(request,'index.html',{})
+
+
+def getCode(request):
+    return custom_render_to_response(request,'getCode.html',{})
+
+def addWebSite(request):
+    return custom_render_to_response(request,'WebSite.html',{})
+
+def saveWebSite(request):
+    return custom_render_to_response(request,'checkWebSite.html',{})
+
+def checkWebSite(request):
+    return custom_render_to_response(request,'addWebSiteSuccess.html',{})
+
+def dataAnalytics(request):
+    return custom_render_to_response(request,'addWebSite.html',{})
+
+def trafficSettings(request):
+    return custom_render_to_response(request,'addWebSite.html',{})
